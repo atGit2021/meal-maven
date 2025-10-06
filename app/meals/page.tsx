80dcd75fc35a5b28a95a328b23bfec4611ead7f1
atGit2021 <a.c.turner.gm@gmail.com>
2025-10-06 16:12:27 -0500

    added meals grid and meal item

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -1,7 +1,9 @@
+import classes from "./page.module.css";
+
 export default function Meals() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col items-center justify-center gap-[8px] row-start-2">
+      <main>
         <h1>Meals Page</h1>
       </main>
     </div>
